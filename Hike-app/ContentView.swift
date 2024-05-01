197fd3e3a7cbfaa47bafc1488141790ffff6754e
raihan <ramadianraihan@gmail.com>
2024-05-01 13:25:45 +0700

    Initial Commit

---
+++ Hike-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike-app
+//
+//  Created by Muhammad Raihan Rahmadian on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike-app/Hike_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_appApp.swift
+//  Hike-app
+//
+//  Created by Muhammad Raihan Rahmadian on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Hike_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
